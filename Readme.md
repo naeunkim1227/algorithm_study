eb8caca6caa1a870718ce42847a860d6ddfb27e0
naeun <kne0140@daum.net>
2022-01-29 22:46:54 +0900

    DAY3 (2022-01-29) Section1- String REPEAT

--- Readme.md
+++ Readme.md
@@ -1,2 +1,24 @@
-## algorithm + 코딩테스트를 위한 repository
+# algorithm + 코딩테스트를 위한 repository
+
+
+## PROGRESS
+
+### DAY1 (2022-01-24) 
+
+문자찾기,대소문자 변환
+
+### DAY2 (2022-01-25)
+
+문장 속 단어,단어 뒤집기
+
+
+### DAY3 (2022-01-29)
+
+DAY1, DAY2 복습
+
+
+
+
+
+
 
---
+++ Section1-String/src/main/java/tochararray/Main.java
@@ -0,0 +1,25 @@
+package tochararray;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public String solution(String str) {
+		String anwser = "";
+		
+		return anwser;
+	}
+	
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		
+		System.out.println(T.solution(str));
+	}
+	
+	
+}
