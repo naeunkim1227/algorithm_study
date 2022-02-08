0874172f6fdac2e7d83cdc549baa23e3188e4b6d
naeun <kne0140@daum.net>
2022-02-08 17:07:51 +0900

    2022-02-08 Section1-String

--- Section1-String/src/main/java/tochararray/Main.java
+++ Section1-String/src/main/java/tochararray/Main.java
@@ -5,36 +5,38 @@ import java.util.Scanner;
 public class Main {
 
 	public String solution(String str) {
-		String anwser = "";
+		String answer = "";
 		
-		//대문자는 65~90
-		//소문자는 97~122
-		//32차이
-		int n  = str.length();
+		//lt,rt 이용하기가 point
 		
-		char[] array = new char[n];
+		char[] s = str.toCharArray();
+		int lt = 0;
+		int rt = str.length() - 1;
 		
-		for(char x : str.toCharArray()) {
-			if(x>=65 && x<=122) {
-				System.out.println(x);
-				
-				
+		while(lt<rt) {
+			//특수문자일때
+			if(!Character.isAlphabetic(s[lt])) lt++;
+			else if(!Character.isAlphabetic(s[rt])) rt--;
+			else {
+				//모두 알파벳일때
+				char tmp = s[lt];
+				s[lt] = s[rt];
+				s[rt] = tmp;
+				lt++;
+				rt--;
 			}
 		}
 		
+		answer = String.valueOf(s);
 		
-		return anwser;
+		return answer;
 	}
 	
 	
 	public static void main(String[] args) {
 		Main T = new Main();
-		
 		Scanner kb = new Scanner(System.in);
-		
-		String str = kb.nextLine();
-		
-		
+		String str = kb.next();
 		System.out.println(T.solution(str));
 	}
 	
